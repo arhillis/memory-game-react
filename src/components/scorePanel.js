9a0f5edde3342163671a67d79494572fe39057e0
Ann Hillis <andriellerhillis@gmail.com>
2019-07-08 14:44:49 -0500

    Added in the timer property

--- src/components/scorePanel.js
+++ src/components/scorePanel.js
@@ -5,20 +5,18 @@ import Stars from './scorePanel/stars';
 class ScorePanel extends Component {
     constructor(props){
         super(props)
-
         this.onClick = props.onClick.bind(this)
     }
     render() {
+        const {moves, timer} = this.props;
         return (
             <div className="score-panel">
                 <Stars />
                 <p class="moves-para" onClick={this.onClick}>
-                    <span className="moves">{this.props.moves}</span> {this.props.moves === 1 
-                                                                            ? 'Move' : 'Moves'} 
+                    <span className="moves">{moves}</span> {moves === 1 ? 'Move' : 'Moves'} 
                 </p>
                 <div className="timer">
-                    <span id="mins">0</span>:
-                    <span id="secs">00</span>
+                    {timer.minutes}: {timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds}
                 </div>
 
                 <div className="restart">
