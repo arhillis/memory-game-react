365e24d332c6d9a7f6e01bc2a9f805208a357b6c
Ann Hillis <andriellerhillis@gmail.com>
2019-07-08 13:23:04 -0500

    Added the header and the score pannel

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ import './App.css';
 function App() {
   return (
     <HashRouter basename="/">
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="container">
+        <header>
+            <h1>Matching Game</h1>
         </header>
+        <div className="score-panel">
+            <ul className="stars">
+                <li>
+                    <i className="fa fa-star gold"></i>
+                </li>
+                <li>
+                    <i className="fa fa-star gold"></i>
+                </li>
+                <li>
+                    <i className="fa fa-star gold"></i>
+                </li>
+            </ul>
+            <p class="moves-para">
+                <span className="moves">0</span> Moves
+            </p>
+            <div className="timer">
+                <span id="mins">0</span>:
+                <span id="secs">00</span>
+            </div>
+
+            <div className="restart">
+                <i className="fa fa-repeat"></i>
+            </div>
+        </div>
       </div>
     </HashRouter>
   );
