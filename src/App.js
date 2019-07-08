cc3acddf24ab34f4a61806d6de7e004c3d7e145c
Ann Hillis <andriellerhillis@gmail.com>
2019-07-08 13:37:03 -0500

    Converted to a class-based component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {HashRouter} from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header';
 import ScorePannel from './components/scorePanel';
 
-function App() {
-  return (
-    <HashRouter basename="/">
-      <div className="container">
-        <Header />
-        <ScorePannel />
-      </div>
-    </HashRouter>
-  );
+class App extends Component{
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div className="container">
+          <Header />
+          <ScorePannel />
+        </div>
+      </HashRouter>
+    );
+  }
 }
+
 
 export default App;
