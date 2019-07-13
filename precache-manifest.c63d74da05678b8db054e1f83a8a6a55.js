59bc186cc05a68b6259c2693e0715f81e7572e99
Ann Hillis <andriellerhillis@gmail.com>
2019-07-13 17:27:31 -0500

    Updates

---
+++ precache-manifest.c63d74da05678b8db054e1f83a8a6a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c62f281592fa47fb22ea6ea8bd38ce",
+    "url": "/memory-game-react/index.html"
+  },
+  {
+    "revision": "e225d3679d25f072aa9d",
+    "url": "/memory-game-react/static/css/main.9b2dbb99.chunk.css"
+  },
+  {
+    "revision": "eccb96363ce1be2e8716",
+    "url": "/memory-game-react/static/js/2.fda31439.chunk.js"
+  },
+  {
+    "revision": "e225d3679d25f072aa9d",
+    "url": "/memory-game-react/static/js/main.187b7dc0.chunk.js"
+  },
+  {
+    "revision": "0735bb19372c7d478a5e",
+    "url": "/memory-game-react/static/js/runtime~main.7d97f3c5.js"
+  }
+]);
